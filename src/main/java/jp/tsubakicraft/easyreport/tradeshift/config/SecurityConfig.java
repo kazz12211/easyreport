c8bc10faaacd23ab5e44458a3667bd59b34d336c
Kazuo Tsubaki <kazz12211@gmail.com>
2018-04-23 11:36:14 +0900

    SecurityConfig changed

--- src/main/java/jp/tsubakicraft/easyreport/tradeshift/config/SecurityConfig.java
+++ src/main/java/jp/tsubakicraft/easyreport/tradeshift/config/SecurityConfig.java
@@ -1,12 +1,13 @@
 package jp.tsubakicraft.easyreport.tradeshift.config;
 
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
-@Order
+@Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     /**
