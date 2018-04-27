20fa744799ede30a6b21691696c9ba45e2c451e6
Kazuo Tsubaki <kazz12211@gmail.com>
2018-04-27 10:12:29 +0900

    tokenService.getRequestHttpEntityWithAccessToken(MediaType.APPLICATION_JSON_UTF8_VALUE)

--- src/main/java/jp/tsubakicraft/easyreport/tradeshift/services/Impl/InvoiceRetrievalServiceImpl.java
+++ src/main/java/jp/tsubakicraft/easyreport/tradeshift/services/Impl/InvoiceRetrievalServiceImpl.java
@@ -131,7 +131,7 @@ public class InvoiceRetrievalServiceImpl implements InvoiceRetrievalService {
 			String[] processStates) throws JSONException {
 		
 		RestTemplate restTemplate = new RestTemplate();
-		HttpEntity<?> requestEntity = 	tokenService.getRequestHttpEntityWithAccessToken(MediaType.APPLICATION_JSON);
+		HttpEntity<?> requestEntity = 	tokenService.getRequestHttpEntityWithAccessToken(MediaType.APPLICATION_JSON_UTF8_VALUE);
 		UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(URI_LIST_DOCUMENTS);
 		builder.queryParam("type", documentType);
 		builder.queryParam("limit", limit);
