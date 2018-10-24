2b34414784c62b1785985351b7e101a8149a6a72
Kazuo Tsubaki <kazz12211@gmail.com>
2018-10-24 11:21:34 +0900

    Modified ApplicationConfig

--- src/main/java/jp/tsubakicraft/easyreport/config/ApplicationConfig.java
+++ src/main/java/jp/tsubakicraft/easyreport/config/ApplicationConfig.java
@@ -2,6 +2,8 @@ package jp.tsubakicraft.easyreport.config;
 
 import java.io.Serializable;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
@@ -10,10 +12,12 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class ApplicationConfig implements Serializable {
 
+	private static Logger LOGGER = LoggerFactory.getLogger(ApplicationConfig.class);
 	FetchLimits fetchLimits;
 	
 	@Autowired
-	protected ApplicationConfig(@Value("{fetchLimit.invoice}") String invoiceFetchLimit) {
+	protected ApplicationConfig(@Value("${fetchLimit.invoice}") String invoiceFetchLimit) {
+		LOGGER.info("fetchLimit.invoice = " + invoiceFetchLimit);
 		fetchLimits = new FetchLimits();
 		try {
 			int limit = Integer.parseInt(invoiceFetchLimit);
